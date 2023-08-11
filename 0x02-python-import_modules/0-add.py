8f36043d43b939d6196fe652e44d32907e63edd6
Atakilti Brhnau <125472106+ABfruit@users.noreply.github.com>
2023-08-11 13:49:23 +0300

    aa

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 
-
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+add_0 = __import__("add_0")
+print("{} + {} = {}".format(a, b, add_0.add(a, b)))
