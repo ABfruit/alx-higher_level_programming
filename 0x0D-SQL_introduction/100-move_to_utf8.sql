93973a9b89c71b4e0ff72306445a76b9ef05139d
Atakilti Brhnau <125472106+ABfruit@users.noreply.github.com>
2023-10-18 13:58:00 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,8 @@
--- Change character SET and COLLATE
--- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
+-- Set the default character set and collation for the entire database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-
-
-
+-- Set the character set and collation for the first_table table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Set the character set and collation for the name field in the first_table table
+ALTER TABLE first_table MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
