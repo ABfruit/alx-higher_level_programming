6f2a0136fc482d781a293646c3e734c8b61ab1ed
Atakilti Brhnau <125472106+ABfruit@users.noreply.github.com>
2023-10-18 13:43:31 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,8 @@
 -- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
 
 
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
